795e5feedb126e6f61dddf109f721e81ee318bec
Zentrik <Zentrik@users.noreply.github.com>
2022-10-28 16:15:28 +0100

    Today's Lecture

---
+++ Methods/heatequationPlot.jl
@@ -0,0 +1,10 @@
+using Plots, LaTeXStrings
+pgfplotsx()
+
+using SpecialFunctions
+surface(-3:.1:3, 0:0.1:20, (x, t) -> 1 * erf(x / (2 * (1 * t)^.5)), camera = (-50,50))
+
+trange = [0; [10^x for x in -3.:2]]
+pl = plot([x -> 1 * erf(x / (2 * (1 * t)^.5)) for t in trange], -3, 3, xlabel=L"x", ylabel=L"\theta", label=[L"t=0" permutedims([L"t = 10^{%$x}" for x in -3:2])], title = L"D=1")
+
+savefig(pl, "$(@__DIR__)/figures/HeatEquation2D.tex")
