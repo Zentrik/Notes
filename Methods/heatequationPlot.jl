24604e0772de3dcb933244bed0edd5a9beacdba9
Zentrik <Zentrik@users.noreply.github.com>
2022-11-01 00:13:55 +0000

    Caught up with lectures, need to insert pictures in methods.

--- Methods/heatequationPlot.jl
+++ Methods/heatequationPlot.jl
@@ -8,3 +8,5 @@ trange = [0; [10^x for x in -3.:2]]
 pl = plot([x -> 1 * erf(x / (2 * (1 * t)^.5)) for t in trange], -3, 3, xlabel=L"x", ylabel=L"\theta", label=[L"t=0" permutedims([L"t = 10^{%$x}" for x in -3:2])], title = L"D=1")
 
 savefig(pl, "$(@__DIR__)/figures/HeatEquation2D.tex")
+
+plot!([x -> 1 * erf(x / (2 * (1 * t)^.5)) for t in trange], -3, 3, label=[L"t=0" permutedims([L"t = 10^{%$x}" for x in -3:2])], title = L"D=1")
